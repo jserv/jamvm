5683ee9a46f438fe10fdb7c01961373c7697b603
Jim Huang <jserv@ccns.ncku.edu.tw>
2023-12-04 00:09:19 +0800

    CI: Bump dependency

--- .ci/openjdk8.sh
+++ .ci/openjdk8.sh
@@ -3,7 +3,7 @@
 . .ci/common.sh
 
 TEMURIN8_URL=https://github.com/adoptium/temurin8-binaries/releases/download
-TEMURIN8_REL=8u352-b08
+TEMURIN8_REL=8u382-b05
 
 curl -L \
     ${TEMURIN8_URL}/jdk${TEMURIN8_REL}/OpenJDK8U-jre_${ARCH}_linux_hotspot_${TEMURIN8_REL/-/}.tar.gz \
